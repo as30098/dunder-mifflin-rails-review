f96ac6b3b03c0bd9f273dd2c9e8655b3a4d08a4f
as30098 <alexspencer421@gmail.com>
2020-12-14 16:44:34 -0500

    completed except for bonus

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,33 @@
 class EmployeesController < ApplicationController
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def new
+    @employee = Employee.new
+    @dogs = Dog.all
+  end
+
+  def create
+    @employee = Employee.create(employee_params)
+    redirect_to employee_path(@employee)
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+    @employee.update(employee_params)
+    redirect_to employee_path(@employee)
+  end
+
+  def employee_params
+    params.require(:employee).permit!
+  end
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,14 @@
 class Dog < ApplicationRecord
+  has_many :employees
+
+  def count_all_employees
+    Dog.all.each do |dog|
+
+    end
+  end
+
+  def employees_first_names
+    
+  end
+
 end
